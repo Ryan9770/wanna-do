48abf38e6a0d3572ecf6fed18e277d7c57a1b4ae
Ryan9770 <zmfh8823@gmail.com>
2021-12-24 16:48:14 +0900

    크리에이터 추가

---
+++ wannaDo/src/main/java/com/bs/wd/admin/memberManage/Creator.java
@@ -0,0 +1,213 @@
+package com.bs.wd.admin.memberManage;
+
+public class Creator {
+	private int listNum;
+	private long memberIdx;
+	private int membership;
+	private String userId;
+	private String userPwd;
+	private String register_date;
+	private String modify_date;
+	private String last_login;
+	private String email;
+	private String email1;
+	private String email2;
+	private String tel;
+	private String tel1;
+	private String tel2;
+	private String tel3;
+	private String birth;
+
+	private int enabled;
+	
+	private long num;
+	private int stateCode;
+	private String memo;
+	private String registerId;
+	private String reg_date;
+	
+	private int creatorIdx;
+	private String intro;
+	private String creatorName;
+	private String imageFilename;
+	private String creatorEmail;
+	private String creatorTel;
+	private String creator_reg_date;
+	
+	public int getListNum() {
+		return listNum;
+	}
+	public void setListNum(int listNum) {
+		this.listNum = listNum;
+	}
+	public long getMemberIdx() {
+		return memberIdx;
+	}
+	public void setMemberIdx(long memberIdx) {
+		this.memberIdx = memberIdx;
+	}
+	public int getMembership() {
+		return membership;
+	}
+	public void setMembership(int membership) {
+		this.membership = membership;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getRegister_date() {
+		return register_date;
+	}
+	public void setRegister_date(String register_date) {
+		this.register_date = register_date;
+	}
+	public String getModify_date() {
+		return modify_date;
+	}
+	public void setModify_date(String modify_date) {
+		this.modify_date = modify_date;
+	}
+	public String getLast_login() {
+		return last_login;
+	}
+	public void setLast_login(String last_login) {
+		this.last_login = last_login;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail1() {
+		return email1;
+	}
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+	public String getEmail2() {
+		return email2;
+	}
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getTel1() {
+		return tel1;
+	}
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	public String getTel2() {
+		return tel2;
+	}
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+	public String getTel3() {
+		return tel3;
+	}
+	public void setTel3(String tel3) {
+		this.tel3 = tel3;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public int getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(int enabled) {
+		this.enabled = enabled;
+	}
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public int getStateCode() {
+		return stateCode;
+	}
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getRegisterId() {
+		return registerId;
+	}
+	public void setRegisterId(String registerId) {
+		this.registerId = registerId;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getCreatorIdx() {
+		return creatorIdx;
+	}
+	public void setCreatorIdx(int creatorIdx) {
+		this.creatorIdx = creatorIdx;
+	}
+	public String getIntro() {
+		return intro;
+	}
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+	public String getCreatorName() {
+		return creatorName;
+	}
+	public void setCreatorName(String creatorName) {
+		this.creatorName = creatorName;
+	}
+	public String getImageFilename() {
+		return imageFilename;
+	}
+	public void setImageFilename(String imageFilename) {
+		this.imageFilename = imageFilename;
+	}
+	public String getCreatorEmail() {
+		return creatorEmail;
+	}
+	public void setCreatorEmail(String creatorEmail) {
+		this.creatorEmail = creatorEmail;
+	}
+	public String getCreatorTel() {
+		return creatorTel;
+	}
+	public void setCreatorTel(String creatorTel) {
+		this.creatorTel = creatorTel;
+	}
+	public String getCreator_reg_date() {
+		return creator_reg_date;
+	}
+	public void setCreator_reg_date(String creator_reg_date) {
+		this.creator_reg_date = creator_reg_date;
+	}
+	
+	
+}
